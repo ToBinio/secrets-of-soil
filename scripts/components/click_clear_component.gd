b12178e3ee594de22571cacf35593f5dd1ca364a
jonas <jonaspfeifer@drei.at>
2025-06-14 21:01:25 +0200

    finished component

--- scripts/components/click_clear_component.gd
+++ scripts/components/click_clear_component.gd
@@ -1,6 +1,13 @@
 extends Area3D
 class_name ClickClearComponent
-	
+
+@export var radius: float
+@onready var shape: CollisionShape3D = $CollisionShape3D
+
+func _ready():
+	shape.shape.radius = radius
+	pass
+
 func _on_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if event is InputEventMouseButton && event.is_pressed() && event.button_index == MOUSE_BUTTON_LEFT:
 		pass
