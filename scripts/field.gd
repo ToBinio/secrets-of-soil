2825d62135e6a15b6496d6873cf07e39d983a115
tobinio <Tobias.frischmann1@gmail.com>
2025-06-14 14:37:08 +0200

    add simple water consumption

--- scripts/field.gd
+++ scripts/field.gd
@@ -1,10 +1,21 @@
 extends Node3D
 
-@export var flower: PackedScene
+@export var flower: Plant
+@export var max_water: float
+
+var _water: float;
+
+func _ready() -> void:
+	_water = max_water
 
 func _on_area_3d_input_event(_camera: Node, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if event is InputEventMouseButton && event.is_pressed() && event.button_index == MOUSE_BUTTON_LEFT:
-		var flower_instance = flower.instantiate()
-		add_child(flower_instance)
-		flower_instance.global_position = event_position
+		if(_water - flower.water_consumption < 0):
+			return
+			
+		_water -= flower.water_consumption;
 		
+		var flower_instance = flower.scene.instantiate()
+		add_child(flower_instance)
+		
+		flower_instance.global_position = event_position
---
+++ scripts/resources/plant.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name Plant
+
+@export var scene: PackedScene
+
+@export var water_consumption: float
