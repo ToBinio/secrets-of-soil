f70f601083a454c15e4f39496f42854477db3064
tobinio <Tobias.frischmann1@gmail.com>
2025-06-14 12:32:50 +0200

    Initial commit

---
+++ scripts/camera.gd
@@ -0,0 +1,40 @@
+extends Camera3D
+
+@export_group("zoom")
+@export var zoom_speed: float = 0.05;  
+@export var zoom_animation_speed: float = 1.0
+
+
+var _plane = Plane(Vector3.UP)
+var _prevous_mouse_location: Vector2;
+
+var _target_zoom: float = position.y
+
+func _process(delta: float) -> void:
+	position.y = lerp(position.y, _target_zoom, zoom_animation_speed * delta)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == MOUSE_BUTTON_LEFT:
+				_prevous_mouse_location = event.position
+			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+				_target_zoom /= 1 + zoom_speed
+			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+				_target_zoom *= 1 + zoom_speed
+
+	if event is InputEventMouseMotion:
+		if event.button_mask == MOUSE_BUTTON_MASK_LEFT:
+			if(!_prevous_mouse_location):
+				return
+			
+			position -= (_get_3d_mouse_postion(event.position) - _get_3d_mouse_postion(_prevous_mouse_location))
+			_prevous_mouse_location = event.position
+
+func _get_3d_mouse_postion(postion: Vector2) -> Vector3:
+	var ray_origin = project_ray_origin(postion)
+	var ray_direction = project_ray_normal(postion)
+	
+	var intersection = _plane.intersects_ray(ray_origin, ray_direction)
+	
+	return intersection;
