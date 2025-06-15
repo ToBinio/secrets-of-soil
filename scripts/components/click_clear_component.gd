4b95e3b73a52ebce14de750fd7314f40dcc263ba
tobinio <Tobias.frischmann1@gmail.com>
2025-06-15 08:15:24 +0200

    show click clear radius in editor

--- scripts/components/click_clear_component.gd
+++ scripts/components/click_clear_component.gd
@@ -1,13 +1,15 @@
+@tool
 extends Area3D
 class_name ClickClearComponent
 
-@export var radius: float
+@export var radius: float :
+	set(value):
+		radius = value
+		
+		if shape:
+			shape.shape.radius = radius
+
 @onready var shape: CollisionShape3D = $CollisionShape3D
 
 func _ready():
 	shape.shape.radius = radius
-	pass
-
-func _on_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
-	if event is InputEventMouseButton && event.is_pressed() && event.button_index == MOUSE_BUTTON_LEFT:
-		pass
