4bb8295fb942197911c097700e72a8c1f731f9b2
tobinio <Tobias.frischmann1@gmail.com>
2025-06-16 19:51:52 +0200

    remove structures if to many

--- scripts/game_manger.gd
+++ scripts/game_manger.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@export var houses: Array[PackedScene]
+@export var structures: Array[PackedScene]
 @export var default_plants: Dictionary[PlantResource, int]
 
 func _ready() -> void:
@@ -14,23 +14,32 @@ func _ready() -> void:
 func _on_next_day():
 	_reset_inventory()
 	
+	_remove_random_structure()
+	_try_generate_random_structure()
+
+func _remove_random_structure():
+	var generated = get_tree().get_nodes_in_group("Generated")
+	
+	if generated.size() < 8:
+		return
+	
+	generated.pick_random().queue_free()
+
+func _try_generate_random_structure():
 	for n in 5:
-		if generate_random_structure():
+		if _generate_random_structure():
 			break
-	
-func generate_random_structure() -> bool:
-	var house = houses.pick_random() as PackedScene;
-	
-	var house_instance = house.instantiate() as Node3D
+
+func _generate_random_structure() -> bool:
+	var structure = structures.pick_random() as PackedScene;
+	var strucuter_instance = structure.instantiate() as Node3D
 	
 	var x = randi_range(-Grid.grid_size,Grid.grid_size + 1)
 	var z = randi_range(-Grid.grid_size,Grid.grid_size + 1)
 	
 	var global_position = Vector3(x * Grid.cell_size, 0.01, z * Grid.cell_size)
 	
-	var plants = Grid.is_valid_location_ignore_plants(global_position, house_instance);
-	
-	print(plants)
+	var plants = Grid.is_valid_location_ignore_plants(global_position, strucuter_instance);
 	
 	if plants is bool && plants == false:
 		return false
@@ -38,10 +47,10 @@ func generate_random_structure() -> bool:
 	for plant in plants:
 		plant.queue_free()
 	
-	add_child(house_instance)
+	add_child(strucuter_instance)
 	
-	house_instance.global_position = global_position
-	house_instance.add_to_group("Generated")
+	strucuter_instance.global_position = global_position
+	strucuter_instance.add_to_group("Generated")
 	
 	return true
 
