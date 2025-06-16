edda77b2e6f1881d44d3ca8a5de23069a9f3a18f
jonas <jonaspfeifer@drei.at>
2025-06-16 12:52:32 +0200

    added basic ui with quest implementation

---
+++ scripts/ui/quest_icon.gd
@@ -0,0 +1,15 @@
+extends Control
+class_name QuestIcon
+
+@export var requirement: QuestRequirementResource:
+	set(value):
+		requirement = value
+
+@onready var label: Label = $QuantityAmount;
+		
+
+func _ready() -> void:
+	set_label_text()
+	
+func set_label_text():
+	label.text = "0/"+str(requirement.required_amount)
---
+++ scripts/ui/quest_type.gd
@@ -0,0 +1,25 @@
+extends TextureRect
+class_name QuestType
+
+@onready var tooltip: Control = $Tooltip;
+@onready var type_name: Label = $Tooltip/Name;
+@onready var type_description: Label = $Tooltip/Description;
+
+
+@export var quest_type: QuestTypeResource:
+	set(value):
+		quest_type = value;
+		set_type()
+
+func set_type():
+	type_name.text = quest_type.name;
+	type_description.text = quest_type.description;
+
+func _on_mouse_entered() -> void:
+	tooltip.visible = true
+	pass # Replace with function body.
+
+
+func _on_mouse_exited() -> void:
+	tooltip.visible = false
+	pass # Replace with function body.
