21f6989f4b154d3847f6ce944b4257c0bc885d83
tobinio <Tobias.frischmann1@gmail.com>
2025-06-17 14:42:12 +0200

    display happiness on hover #14

--- scripts/components/clicker_area.gd
+++ scripts/components/clicker_area.gd
@@ -4,12 +4,28 @@ class_name ClickerArea
 signal on_click
 
 @export var growable: Growable
+@export var hover_label: Node3D
+
+var fully_grown = false
 
 func _ready() -> void:
 	if(growable):
-		input_ray_pickable = false
-		growable.on_fully_grown.connect(func(): input_ray_pickable = true)
+		growable.on_fully_grown.connect(func(): fully_grown = true)
+	
+	if hover_label:
+		hover_label.hide()
 
 func _on_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	if !fully_grown:
+		return
+		
 	if event is InputEventMouseButton && event.is_pressed() && event.button_index == MOUSE_BUTTON_LEFT:
 		on_click.emit()
+
+func _on_mouse_entered() -> void:
+	if hover_label:
+		hover_label.show()
+
+func _on_mouse_exited() -> void:
+	if hover_label:
+		hover_label.hide()
--- scripts/components/growable.gd
+++ scripts/components/growable.gd
@@ -5,6 +5,7 @@ signal on_fully_grown
 
 @export var plant: Plant
 @export var visual: Node3D
+@export var happiness_label: Label3D
 
 var target_size = 0.1
 
@@ -15,7 +16,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	visual.scale = lerp(visual.scale, Vector3.ONE *  target_size, delta * 5);
 
-func _on_next_day():
+func _calc_growth_factor() -> float:
 	var grid_pos = Grid.to_grid_cord(plant.global_position)
 	var water_level = Grid.get_water_percentage(grid_pos)
 	
@@ -25,6 +26,15 @@ func _on_next_day():
 	water_level = clamp(water_level,0,1)
 	
 	var growth_factor = 1 - abs(plant.plant.preferred_water - water_level)
+	
+	return growth_factor
+
+func _on_next_day():
+	var growth_factor = _calc_growth_factor()
+	
+	if happiness_label:
+		happiness_label.text = "Happy: %.2f" % growth_factor
+	
 	target_size = min(target_size + growth_factor * plant.plant.growth_speed, 1)
 	
 	if(target_size == 1):
