53c83a34811329bb50c5dc20cc5651b56f53c547
tobinio <Tobias.frischmann1@gmail.com>
2025-06-18 20:56:42 +0200

    spawn houses in cluster

--- scripts/game_manger.gd
+++ scripts/game_manger.gd
@@ -15,11 +15,16 @@ var current_food_requirements: int;
 var _current_weather: int
 
 @export var possible_weather: Array[WeatherResource]
-@export var structures: Array[PackedScene]
+
+@export var houses: Array[PackedScene]
+@export var housing_radius: int
+@export var water_sources: Array[PackedScene]
+@export_range(0,1) var water_source_change: float
 
 @export var default_plants: Dictionary[PlantResource, int]
 
 @export var death_screen: Control
+@export var can_die: bool
 
 static func instant(node: Node) -> GameManager:
 	return node.get_tree().get_first_node_in_group("GameManager") as GameManager
@@ -41,7 +46,7 @@ func _ready() -> void:
 	Events.on_next_day.connect(_on_next_day)
 
 func _on_next_day():
-	if !_consume_food():
+	if !_consume_food() && can_die:
 		death_screen.show()
 		return
 	
@@ -104,33 +109,67 @@ func _remove_random_structure():
 
 func _try_generate_random_structure():
 	for n in 5:
-		if _generate_random_structure():
-			break
+		if randf() < water_source_change:
+			if _generate_random_water_source(): return
+		else:
+			if _generate_random_housing(): return
 
-func _generate_random_structure() -> bool:
-	var structure = structures.pick_random() as PackedScene;
-	var strucuter_instance = structure.instantiate() as Node3D
+func _generate_random_housing() -> bool:
+	var structure = houses.pick_random() as PackedScene;
 	
 	var x = randi_range(-Grid.grid_size,Grid.grid_size + 1)
 	var z = randi_range(-Grid.grid_size,Grid.grid_size + 1)
+	
+	var result =  _try_spawn_structure(structure, x, z, self);
+	
+	if not result[0]:
+		return false
+	
+	result[1].add_to_group("Generated")
+	
+	for i in 5:
+		var structure_addition = houses.pick_random() as PackedScene;
+		
+		var x_addition = randi_range(x - housing_radius, x + housing_radius)
+		var z_addition = randi_range(z - housing_radius, z + housing_radius)
+		
+		_try_spawn_structure(structure_addition, x_addition, z_addition, result[1])
+	
+	return true
+
+func _generate_random_water_source() -> bool:
+	var structure = water_sources.pick_random() as PackedScene;
+	
+	var x = randi_range(-Grid.grid_size,Grid.grid_size + 1)
+	var z = randi_range(-Grid.grid_size,Grid.grid_size + 1)
+	
+	var result = _try_spawn_structure(structure, x, z, self)
+	
+	if not result[0]:
+		return false
+	
+	result[1].add_to_group("Generated")
+	return true
+
+func _try_spawn_structure(structure: PackedScene, x: int, z: int, parent: Node):
+	var strucuter_instance = structure.instantiate() as Node3D
 	
 	var global_position = Vector3(x * Grid.cell_size, 0.01, z * Grid.cell_size)
 	
 	var plants = Grid.is_valid_location_ignore_plants(global_position, strucuter_instance);
 	
 	if plants is bool && plants == false:
-		return false
+		return [false, null]
 	
 	for plant in plants:
 		stats.plants_destroyed_by_village += 1
 		plant.queue_free()
 	
-	add_child(strucuter_instance)
+	parent.add_child(strucuter_instance)
 	
 	strucuter_instance.global_position = global_position
-	strucuter_instance.add_to_group("Generated")
 	
-	return true
+	return [true, strucuter_instance]
 
 func _reset_inventory():
 	for key in Inventory.plants.keys():
