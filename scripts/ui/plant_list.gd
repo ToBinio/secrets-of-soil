c63a4bad3cfa633fab4147a107d851eaa6b72ef2
jonas <jonaspfeifer@drei.at>
2025-06-21 07:49:54 +0200

    knowledge book and plant selector done

--- scripts/ui/plant_list.gd
+++ scripts/ui/plant_list.gd
@@ -2,7 +2,7 @@ extends Control
 
 @export var plant_container: PackedScene
 
-@onready var plant_box: HBoxContainer = $ScrollContainer/HBoxContainer
+@onready var plant_box: GridContainer = $Background/GridContainer;
 
 func _ready() -> void:
 	for plant in Plants.all_plants():
