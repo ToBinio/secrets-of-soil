db8587eeb9d5550231490aeff3593e8e93e0383b
jonas <jonaspfeifer@drei.at>
2025-06-22 21:20:39 +0200

    added notification sound

--- scripts/ui/knowledge_book_button.gd
+++ scripts/ui/knowledge_book_button.gd
@@ -5,9 +5,19 @@ class_name KnowledgeBookButton
 @onready var info: Control = $Info
 
 @export var click_sound: AudioStream;
+@export var notification_sound: AudioStream;
 
 func _ready() -> void:
-	Events.on_new_knowledge.connect(func(): info.show())
+	Events.on_new_knowledge.connect(func(): 
+		info.show()
+		
+		var player = AudioStreamPlayer2D.new();
+		player.volume_db = -10;
+		player.stream = notification_sound;
+		get_tree().root.add_child(player)
+		player.playing = true
+		player.finished.connect(func(): player.queue_free())
+	)
 	info.hide()
 
 func _on_button_down() -> void:
